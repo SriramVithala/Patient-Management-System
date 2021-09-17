85ccdadc82c57b72f1c2f16cee0809a6935d61bb
SriramVithala <82681388+SriramVithala@users.noreply.github.com>
2021-09-17 10:36:59 -0400

    Add files via upload

---
+++ PS3/IPatient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS3
+{
+    interface IPatient
+    {
+        int Patientid { get; set; }
+        string Patientname { get; set; }
+        
+
+        void getDetails();
+        void printDetails();
+    }
+}
---
+++ PS3/Patient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS3
+{
+    abstract class Patient : IPatient
+    {
+        int patientid;
+        string patientname;
+        Address PatientAddress;
+
+        public int Patientid
+        {
+            get
+            {
+                return patientid;
+            }
+            set
+            {
+                patientid = value;
+            }
+        }
+        public string Patientname
+        {
+            set
+            {
+                patientname = value;
+            }
+            get
+            {
+                return patientname;
+            }
+        }
+        
+
+        public abstract void getDetails();
+        public abstract void printDetails();
+
+    }
+}
